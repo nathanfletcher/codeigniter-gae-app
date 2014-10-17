4bc70f5284cc3a050894bdd2e6aa1f330984968f
Vanga Sasidhar <sasidhar@sasidhar.com>
2014-10-17 09:10:31 +0530

    Extended CI controller with autentication related code.

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,41 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class MY_Controller extends CI_Controller {
+
+    var $request_method;
+
+    public function __construct() {
+
+        parent::__construct();
+
+        //Init
+        $this->_init();
+    }
+
+    private function _init() {
+
+        //Load base user model
+        $this->load->model('users/base_user_model', 'base_user_model');
+
+        //Load different models and libraries based on current site
+        $site = $this->uri->segment(1);
+
+        switch($site) {
+            case "super-admin" :
+                $this->load->model('users/super_admins_model');
+                $this->load->library('authentication/super_admin_authentication_controller', '', 'super_admin_auth');
+                break;
+            case "admin" :
+                $this->load->model('users/admins_model');
+                $this->load->library('authentication/admin_authentication_controller', '', 'admin_auth');
+                break;
+            default:
+                $this->load->model('users/users_model');
+                $this->load->library('authentication/user_authentication_controller', '', 'user_auth');
+                break;
+        }
+
+        //Store current request method for later use
+        $this->request_method = strtolower($_SERVER['REQUEST_METHOD']);
+    }
+}
