652f18cd15035d38a7d2b435297511cc75b97cde
Vanga Sasidhar <sasidhar@sasidhar.com>
2014-10-19 11:54:16 +0530

    Added sample SMTP Configuration file.

---
+++ application/config/smtp.sample.php
@@ -0,0 +1,9 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['custom_smtp'] = array(
+    'protocol' => 'smtp',
+    'smtp_host' => 'smtp_host',
+    'smtp_port' => 2525,
+    'smtp_user' => 'smtp_user',
+    'smtp_pass' => 'smtp_pass'
+);
