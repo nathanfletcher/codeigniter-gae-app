93d615654c91b056788831d456ac0a38f4d78a93
Vanga Sasidhar <sasidhar@sasidhar.com>
2014-10-20 23:39:30 +0530

    Deleted unwanted smtp configuration file.

--- application/config/smtp.sample.php
+++
@@ -1,9 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$config['custom_smtp'] = array(
-    'protocol' => 'smtp',
-    'smtp_host' => 'smtp_host',
-    'smtp_port' => 2525,
-    'smtp_user' => 'smtp_user',
-    'smtp_pass' => 'smtp_pass'
-);
